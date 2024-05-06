358a78dd90b3d124ce3686faf000189131e45481
Bubesz11 <liloo1121@freemail.hu>
2024-05-06 20:31:44 +0200

    Add files via upload

---
+++ blackjack.js
@@ -0,0 +1,24 @@
+@media only screen and (max-width: 1195px) {
+    body {
+      background-image: url('https://static.vecteezy.com/system/resources/previews/002/582/107/original/poker-black-background-playing-card-symbols-pattern-blackjack-vector.jpg');
+      background-repeat:calc();
+    }
+  }
+  
+  @media only screen and (max-width: 591px) {
+      #dealer-cards img {
+          height: 100px;
+                  width: 80px;
+                  margin: 20px;
+                  transform: rotate(-20deg);
+    }
+  }
+  
+  @media only screen and (max-width: 591px) {
+      #your-cards img {
+          height: 100px;
+                  width: 80px;
+                  margin: 20px;
+                  transform: rotate(-20deg);
+    }
+  }
